4af8a5da91173a64501598b392cd779f10c12aa5
Pratiksha Pundpal <77484588+PratikshaDPundpal@users.noreply.github.com>
2024-11-18 00:36:54 +0530

    Update serviceRoutes.js

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const serviceController = require('../controllers/serviceController');
 
+// Define routes for services
+router.post('/services', serviceController.createService);
+router.get('/services', serviceController.getAllServices);
+router.put('/services/:id', serviceController.updateService);
+router.delete('/services/:id', serviceController.deleteService);
+
+module.exports = router;
